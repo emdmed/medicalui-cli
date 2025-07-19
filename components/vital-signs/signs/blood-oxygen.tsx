7b989817253355159d830a1ec53a0492a4fff9a4
emdmed <enrique.darderes@gmail.com>
2025-07-19 19:52:50 +0200

    update blood oxygen style to match other vital signs

--- components/vital-signs/signs/blood-oxygen.tsx
+++ components/vital-signs/signs/blood-oxygen.tsx
@@ -181,7 +181,7 @@ const BloodOxygen: React.FC<BloodOxygenProps> = ({
         <div className="flex items-center gap-2">
           <div className="flex items-center">
             {bloodOxygenValue !== null ? (
-              <span className="flex items-baseline gap-2">
+              <span className="flex items-baseline gap-2  hover:text-accent-foreground transition-all">
                 {bloodOxygenValue}{" "}
                 <small className="flex items-baseline gap-2">
                   <small className="opacity-50">%</small>
